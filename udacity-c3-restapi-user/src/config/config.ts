8bc811d53c91711c43c8d0788ce388b2e1adf6dc
EC2 Default User <ec2-user@ip-172-31-34-11.us-east-2.compute.internal>
2020-05-02 10:40:48 +0000

    2nd commit

---
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+  "dev": {
+    "username": "udagramrahmandev",
+    "password": "udagramrahmandev",
+    "database": "udagramrahmandev",
+    "host": "udagramrahmandev.cxtzrsndua2k.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    
+    
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-rahman-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret":"helloworld"
+  }
+}
